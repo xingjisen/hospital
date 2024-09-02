6ccbc13b16c64fed527a7c8e6aa90a4de5eca9f9
邢继森 <1933694610@qq.com>
2024-09-02 21:04:51 +0800

    V0.0.3 配置初始化

---
+++ service/service_hosp/src/main/java/com/yygh/hosp/ServiceHospApplication.java
@@ -0,0 +1,17 @@
+package com.yygh.hosp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jason
+ * @PACKAGE_NAME com.yygh.hosp
+ * @Description
+ * @date 2024-09-02 20:43
+ */
+@SpringBootApplication
+public class ServiceHospApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHospApplication.class, args);
+    }
+}
