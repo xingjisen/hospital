f8811b6761f46989de3025d3cc194846f0823d3d
邢继森 <1933694610@qq.com>
2024-09-29 21:50:21 +0800

    V0.1.0 gateway全局配置跨域方法(未启用);

---
+++ service_gateway/src/main/java/com/yygh/gateway/config/CrosConfig.java
@@ -0,0 +1,29 @@
+package com.yygh.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @author Jason
+ * @PACKAGE_NAME com.yygh.gateway.config
+ * @Description 解决跨域问题
+ * @date 2024-09-29 10:16
+ */
+//@Configuration
+public class CrosConfig {
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(source);
+    }
+
+}
